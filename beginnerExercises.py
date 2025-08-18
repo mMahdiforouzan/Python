e73a454021000846384a65b4d09ca5b6a6a64789
Mohammadmahdi  Forouzan <mmahdi.forouzan@gmail.com>
2025-08-18 19:38:07 -0400

    added more exercises

--- beginnerExercises.py
+++ beginnerExercises.py
@@ -18,4 +18,25 @@ def exercise2():
         print("Current number: %d Previous number: %d sum: %d"%(x,x0,x+x0))
         x0=x
         x=x+1
-        
+    return
+
+
+#Write a Python code to accept a string from the user and display characters present at an even index number.
+def exercise3(stringValue):
+    for x in range(0,len(stringValue),2):
+        print(stringValue[x])
+
+
+#Write a Python code to remove characters from a string from 0 to n and return a new string.
+def exercise4(word,n):
+    return word[n:]
+
+#Write a code to return True if the list’s first and last numbers are the same. If the numbers are different, return False.
+def exercise5(thisList):
+    return thisList[0]==thisList[len(thisList)-1]
+
+#Write a Python code to display numbers from a list divisible by 5
+def exercise6(thisList):
+    for x in range(len(thisList)-1):
+        if((thisList[x]%5)==0):
+            print(thisList[x])
--- main.py
+++ main.py
@@ -2,16 +2,43 @@ import beginnerExercises as beginnerExercises
 import random as random
 
 print("select which exercise")
-input = input()
+inputValue = int(input())
 
-match input: 
-   case "1":
-    for counter in range(10):
-        x = random.randint(0,50)
-        y = random.randint(0,50)
-        z = beginnerExercises.exercise1(x,y)         
-        print("x   y   sum product answer")
-        print("%d  %d  %d    %d      %d"%(x,y,x+y,x*y,z))
-   case "2":
+match inputValue: 
+     case 1:
+          for counter in range(10):
+            x = random.randint(0,50)
+            y = random.randint(0,50)
+            z = beginnerExercises.exercise1(x,y)         
+            print("x   y   sum product answer")
+            print("%d  %d  %d    %d      %d"%(x,y,x+y,x*y,z))
+     case 2:
         beginnerExercises.exercise2()
+
+     case 3:
+        print("give a string!")
+        stringValue = input()
+        beginnerExercises.exercise3(stringValue)
+
+     case 4:
+        print("give a string!")
+        word = input()
+        print("give a number!")
+        n = int(input())
+        print(beginnerExercises.exercise4(word,n))
+
+     case 5 | 6:
+        counter = 0
+        thisList = []
+        thisList.append(int(input()))
+        while(thisList[len(thisList)-1]>=0):
+               thisList.append(int(input()))
+     
+        thisList.pop(len(thisList)-1)
+        if inputValue == 5:
+          print(beginnerExercises.exercise5(thisList))
+        if inputValue == 6:
+            beginnerExercises.exercise6(thisList)
+        
+    
     
